5f9c2df6d79aedf199cdb6d7b493cb05d4c6950e
hyogyu <68395256+hyogyu@users.noreply.github.com>
2020-07-17 21:57:31 +0900

    20200717-3
    1. 연산자 사용 / 출력

--- src/com/hyogyu/Practice/Java_20200717.java
+++ src/com/hyogyu/Practice/Java_20200717.java
@@ -24,8 +24,24 @@ public class Java_20200717 {
 		// 실수 | 4 Byte | 32 Bit 실수
 		// 실수 | 8 Byte | 64 Bit 실수
 		// 불린 | 1 Byte | True / False
-		
+
 		System.out.println("char : " + c + "\nbyte : " + b + "\nshort : " + s + "\nint : " + i + "\nlong : " + l
 				+ "\nfloat : " + f + "\ndouble : " + d + "\nboolean : " + bool);
+
+		// 연산자
+		int i1 = 0;
+		int i2 = +10;
+		int i3 = -10;
+
+		System.out.println(i1 + " / " + i2 + " / " + i3); // 0 | 10 | -10
+
+		int i4 = ++i1;
+		System.out.println(i4 + " / " + i1); // 1 | 1
+		int i5 = i1++;
+		System.out.println(i5 + " / " + i1); // 1 | 2
+
+		int x = 10;
+		int y = 5;
+		System.out.println((x + y) + " / " + (x - y) + " / " + (x * y) + " / " + (x / y) + " / " + (x % y)); // 15 | 5 | 50 | 2 | 0
 	}
 }
